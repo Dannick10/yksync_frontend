79f496461aebafe2a1a2caad94a3b91a8b76550e
Dannick010 <danmaggot2@gmail.com>
2025-04-29 11:37:23 -0300

    fix/responsive and fix animation

--- src/app/(public)/components/FeaturedProject.tsx
+++ src/app/(public)/components/FeaturedProject.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React, { useRef } from "react";
-import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import { motion, useScroll, useSpring } from "framer-motion";
+import { FaChevronDown } from "react-icons/fa";
 
 type featureProject = {
   title: string;
@@ -36,6 +37,7 @@ const FeaturedProject = () => {
     },
   ];
 
+  const projectsRef = useRef<(HTMLDivElement | null)[]>([]);
   const containerRef = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: containerRef,
@@ -48,12 +50,25 @@ const FeaturedProject = () => {
     restDelta: 0.001,
   });
 
+  const scrollToProject = (index: number) => {
+    projectsRef.current[index]?.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+  };
+
+  const handleNext = (currentIndex: number) => {
+    const nextIndex = (currentIndex + 1) % featureProjects.length;
+    scrollToProject(nextIndex);
+  };
+
+
+
   return (
     <>
-      <section className="relative">
-
+      <section className="relative ">
         <div className="absolute top-0 left-0 h-2 w-full bg-zinc-50 z-20 "></div>
-      
+
         <motion.div className="sticky container mx-auto top-20 rounded-md left-0 w-full h-2 bg-zinc-500 z-10 overflow-hidden">
           <motion.div
             className="w-full h-2 bg-zinc-900 z-20 origin-left"
@@ -61,54 +76,58 @@ const FeaturedProject = () => {
           />
         </motion.div>
 
-        <div ref={containerRef} className="container mx-auto px-4 py-10">
+        <div ref={containerRef} className="container mx-auto px-4 py-10 ">
           <div className="space-y-20">
             {featureProjects.map((project, i) => {
               const delay = i * 0.15;
+              const OneByOne = i % 2 == 0;
+
               return (
-                <motion.div
+                <div
                   key={i}
-                  className={`flex flex-col-reverse ${i % 2 == 0 ? "md:flex-row" : "md:flex-row-reverse"} gap-8 items-center justify-center sticky top-40 md:top-20 left-0 bg-zinc-50 h-screen`}
-                  initial={{ opacity: 0, y: 50 }}
-                  whileInView={{ opacity: 1, y: 0 }}
-                  transition={{
-                    duration: 0.8,
-                    ease: [0.16, 0.77, 0.47, 0.97],
-                    delay,
+                  ref={(el) => {
+                    if (el) projectsRef.current[i] = el;
                   }}
+                  className={`flex flex-col-reverse ${OneByOne ? "md:flex-row" : "md:flex-row-reverse"} gap-8 items-center justify-center sticky top-40 md:top-20 left-0 bg-zinc-50 h-screen`}
                 >
                   <motion.div
                     className="flex-1 flex flex-col gap-4 relative"
-                    initial={{ x: i % 2 === 0 ? -50 : 50 }}
-                    whileInView={{ x: 0 }}
-                    transition={{ duration: 0.6, delay: delay + 0.2 }}
+                    initial={{ x: OneByOne ? -10 : 10, opacity: 0 }}
+                    whileInView={{ x: 0, opacity: 1 }}
+                    transition={{
+                      duration: 0.6,
+                      ease: "linear",
+                      delay: delay + 0.1,
+                    }}
                   >
                     <h2 className="text-3xl md:text-5xl font-bold text-gray-950">
                       {project.title}
                     </h2>
                     <p className="text-zinc-600 text-lg">{project.subtitle}</p>
 
-                    <motion.div
-                      className="absolute -top-16 left-0 bg-black w-10 h-10 flex justify-center items-center font-bold rounded-full text-white z-10"
-                      initial={{ scale: 2, opacity: 0 }}
-                      whileInView={{ scale: 1, opacity: 1 }}
-                      transition={{
-                        duration: 0.6,
-                        ease: "backOut",
-                        delay: delay + .8,
-                      }}
-                    >
+                    <div className="absolute -top-16 left-0 bg-black w-10 h-10 flex justify-center items-center font-bold rounded-full text-white z-10">
                       {i + 1}
-                    </motion.div>
+                    </div>
+                    <div
+                      className={`flex items-center ${OneByOne ? "md:items-end" : "md:items-start"} flex-col ${i === featureProjects.length -1 && 'hidden'}`}
+                    >
+                      <button
+                        onClick={() => handleNext(i)}
+                        className="p-2 hover:bg-gray-200 rounded-full text-xl bg-black text-white"
+                        aria-label="Próximo projeto"
+                      >
+                        <FaChevronDown />
+                      </button>
+                    </div>
                   </motion.div>
 
                   <motion.div
                     className=""
-                    initial={{ scale: 0.9, opacity: 0 }}
+                    initial={{ scale: 0.7, opacity: 0 }}
                     whileInView={{ scale: 1, opacity: 1 }}
                     transition={{
-                      duration: 0.6,
-                      ease: "backOut",
+                      duration: 0.3,
+                      ease: "linear",
                       delay: delay + 0.1,
                     }}
                   >
@@ -123,7 +142,7 @@ const FeaturedProject = () => {
                       />
                     </div>
                   </motion.div>
-                </motion.div>
+                </div>
               );
             })}
           </div>
